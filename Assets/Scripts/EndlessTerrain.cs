0829b1e071dfd5a48ab45d62670fcd1f6afe5c3a
Christian Obed Arana navas <COANelmejor@gmail.com>
2021-06-11 00:35:08 -0600

    Procedural Landmass Generation (E07: Endless terrain) Terminado

---
+++ Assets/Scripts/EndlessTerrain.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessTerrain : MonoBehaviour
+{
+    public const float maxViewDistance = 450f;
+    public Transform viewer;
+
+    public static Vector2 viewerPosition;
+    int chunkSize;
+    int chunksVisibleInViewDistance;
+
+    Dictionary<Vector2, TerrainChunk> terrainChunkDictionary = new Dictionary<Vector2, TerrainChunk>();
+    List<TerrainChunk> terrainVisibleLastUpdate = new List<TerrainChunk>();
+
+    void Start()
+    {
+        chunkSize = MapGenerator.mapChunkSize - 1;
+        chunksVisibleInViewDistance = Mathf.RoundToInt(maxViewDistance / chunkSize);
+    }
+
+    private void Update()
+    {
+        viewerPosition = new Vector2(viewer.position.x, viewer.position.z);
+        UpdateVisibleChunks();
+    }
+
+    void UpdateVisibleChunks()
+    {
+        for (int t = 0; t < terrainVisibleLastUpdate.Count; t++)
+        {
+            terrainVisibleLastUpdate[t].SetVisible(false);
+        }
+        terrainVisibleLastUpdate.Clear();
+
+        int currentChunkCoordX = Mathf.RoundToInt(viewerPosition.x / chunkSize);
+        int currentChunkCoordY = Mathf.RoundToInt(viewerPosition.y / chunkSize);
+
+        for (int yOffset = -chunksVisibleInViewDistance; yOffset <= chunksVisibleInViewDistance; yOffset++) 
+        {
+            for (int xOffset = -chunksVisibleInViewDistance; xOffset <= chunksVisibleInViewDistance; xOffset++)
+            {
+                Vector2 viewedChunkCoord = new Vector2(currentChunkCoordX + xOffset, currentChunkCoordY + yOffset);
+
+                if(terrainChunkDictionary.ContainsKey(viewedChunkCoord))
+                {
+                    terrainChunkDictionary[viewedChunkCoord].UpdateTerrainChunk();
+                    if (terrainChunkDictionary[viewedChunkCoord].IsVisible())
+                    {
+                        terrainVisibleLastUpdate.Add(terrainChunkDictionary[viewedChunkCoord]);
+                    }
+                } else
+                {
+                    terrainChunkDictionary.Add(viewedChunkCoord, new TerrainChunk(viewedChunkCoord, chunkSize, transform));
+                }
+            }
+
+        }
+    }
+
+    public class TerrainChunk
+    {
+        GameObject meshObject;
+        Vector2 position;
+        Bounds bounds;
+
+        public TerrainChunk(Vector2 coord, int size, Transform parent)
+        {
+            position = coord * size;
+            bounds = new Bounds(position, Vector2.one * size);
+            Vector3 positionV3 = new Vector3(position.x, 0, position.y);
+
+            meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            meshObject.transform.position = positionV3;
+            meshObject.transform.localScale = Vector3.one * size / 10f;
+            meshObject.transform.parent = parent;
+
+            SetVisible(false);
+        }
+
+        public void UpdateTerrainChunk()
+        {
+            float viewerDistanceFromNearestEdge = Mathf.Sqrt (bounds.SqrDistance(viewerPosition));
+            bool visible = viewerDistanceFromNearestEdge <= maxViewDistance;
+            SetVisible(visible);
+        }
+
+        public void SetVisible(bool visible)
+        {
+            meshObject.SetActive(visible);
+        }
+
+        public bool IsVisible ()
+        {
+            return meshObject.activeSelf;
+        }
+    }
+}
